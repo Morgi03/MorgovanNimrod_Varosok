2a45dfcbdc168d2b4265cacfc6b75e3aff2f58d1
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-25 08:52:20 +0100

    ListActivity kinézetének elkészítése

--- app/src/main/java/hu/petrik/varosok/ListActivity.java
+++ app/src/main/java/hu/petrik/varosok/ListActivity.java
@@ -2,13 +2,28 @@ package hu.petrik.varosok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
+    private Button backBtn;
+    private TextView datainJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        backBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(ListActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void init() {
+        backBtn = findViewById(R.id.backBtn);
+        datainJson = findViewById(R.id.datainJson);
     }
 }
