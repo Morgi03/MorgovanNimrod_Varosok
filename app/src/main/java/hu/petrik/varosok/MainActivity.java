2977ba3cb6536f85dfd978483ffee3474c9e3422
Morgován Nimród <morgovannimrod22@gmail.com>
2022-11-25 08:43:54 +0100

    Másik két activity-re való átlépés megírása

--- app/src/main/java/hu/petrik/varosok/MainActivity.java
+++ app/src/main/java/hu/petrik/varosok/MainActivity.java
@@ -2,13 +2,33 @@ package hu.petrik.varosok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button listBtn;
+    private Button newitemBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        listBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+            startActivity(intent);
+            finish();
+        });
+        newitemBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void init() {
+        listBtn = findViewById(R.id.listBtn);
+        newitemBtn = findViewById(R.id.newitemBtn);
     }
 }
